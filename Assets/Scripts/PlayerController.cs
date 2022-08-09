cadb61a542afd6441fa6e2424a773c1eb2d7d115
Bas de Ruiter <bas@basbv.it>
2022-08-09 21:42:01 +0200

    Complete Lesson 3.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce = 10;
+    private Animator playerAnim;
+    public float jumpForce = 800;
     public float gravityModifier = 1.5f;
     public bool isOnGround = true;
     public bool gameOver;
@@ -15,14 +16,17 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && ! gameOver) {
             playerRb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
         }
     }
@@ -31,8 +35,11 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground")) {
             isOnGround = true;
-        } else if (collision.gameObject.CompareTag("Obstacle")) {
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")) {
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             gameOver = true;
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,7 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstacle;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private float startDelay = 2;
+    private float startDelay = 1;
     private float repeatRate = 2;
     private PlayerController playerController;
     
